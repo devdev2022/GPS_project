93b05213bd94c93d28acf35ce047244adb9fed1d
khs <devdev0226@gmail.com>
2023-06-07 17:03:39 +0900

    reservationRouter 수정

--- controllers/driverController.js
+++ controllers/driverController.js
@@ -7,18 +7,23 @@ const acceptReservation = async (req, res) => {
         await driverService.acceptReservation(reservation_id, driver_id);
         res.status(200).json({ message: 'Reservation accepted.' });
     } catch (error) {
-        console.log(error)
         res.status(500).json({ message: 'An error occurred.' });
     }
 };
 
 const getclosereservations = async (req, res) => {
+    const driverId = req.params.driver_id;
     const driverLocation = {
         latitude: req.query.latitude,
         longitude: req.query.longitude
     };
 
     try {
+        const driver = await driverService.getDriverById(driverId);
+        if (!driver) {
+            return res.status(404).json({ message: 'USER_DOES_NOT_EXIST' }); 
+        }
+        
         const reservations = await driverService.getclosereservations(driverLocation);
         res.status(200).json(reservations);
     } catch (error) {
--- routes/reservationRouter.js
+++ routes/reservationRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { userLoginRequired, driverLoginRequired } = require('../utils/checkUser');
 const { requestReservation, getReservations } = require('../controllers/userController');
-const { acceptReservation } = require('../controllers/driverController');
+const { acceptReservation, getclosereservations } = require('../controllers/driverController');
 
 // 유저 예약 요청
 router.post('/:user_id', userLoginRequired, requestReservation);
@@ -14,6 +14,6 @@ router.get('/:user_id', userLoginRequired, getReservations);
 router.post('/:reservation_id/:driver_id', driverLoginRequired, acceptReservation);
 
 // 드라이버 예약 목록 요청 
-router.get('/reservations', getReservations);
+router.get('/:driver_id', driverLoginRequired, getclosereservations);
 
 module.exports = router;
